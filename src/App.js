15e4da3751a5403c8728b26e6eb73c6a12622051
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-12 17:41:11 -0700

    added loading component

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
   //   },
   // ];
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
     const transformedMovies = data.results.map((data) => {
@@ -31,6 +33,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -39,7 +42,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies...</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
